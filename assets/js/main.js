27a81b9beb0cfa7b3a030a92ae0a0b274409ff38
Carl Centola <carl.centola@gmail.com>
2018-02-21 19:58:57 -0500

    added the ability to remove todos

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,3 +2,15 @@
 $("li").click(function(){
     $(this).toggleClass("completed");
 });
+
+// click on x to delete todo
+$("span").click(function(event){
+    
+    // remove elements by clicking on x
+    $(this).parent().fadeOut(500,function(){
+        $(this).remove();
+    });
+    
+    // stop click from effecting parent elements
+    event.stopPropagation;
+});
